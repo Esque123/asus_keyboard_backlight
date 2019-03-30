32ca7ad57272ce3d2ab0b0453890b134d5b84d08
Kevin <mostert.k@gmail.com>
2019-03-30 23:07:50 +0200

    Mostly Final script
    Could someday add a "if" statement to set the value of the back-light to 0 and disable the back-light.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Keyboard_Backlight
+
+A script for ASUS G551JW to load at startup to set my keyboard backlight on or used with crontab to schedule the time to enable the backlight
+
---
+++ keyboard_backlight.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A script for ASUS G551JW to load at startup to set my keyboard backlight on or used with crontab to schedule the time.
+# Kevin Mostert
+# 27/03/2019
+
+#Sets the value of the brightness level. (Can use 1-3)
+echo 1 | tee /sys/class/leds/asus::kbd_backlight/brightness
+echo "Keyboard backlight enabled at $(date)" >> /home/kevin/scripts.log
+notify-send "Keyboard Backlight enabled" --urgency=normal
